d9d15a0242a392e59a6071a9d9b33b6df487f2e2
Matias Lamela <matiaslamela1992@gmail.com>
2021-03-02 12:48:39 -0300

    initial config

--- api/src/models/Hero.js
+++ api/src/models/Hero.js
@@ -4,7 +4,7 @@ const {DataTypes} = require('sequelize');
 module.exports = (sequelize) => {
 	// defino el modelo
 	sequelize.define('hero', {
-		name: {
+		nombre: {
 			type: DataTypes.STRING,
 			allowNull: false,
 		},
